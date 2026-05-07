7ada6ccd8316b3c6038f55991a6f0a38d620b828
Ruan Amorim de Mendonça <150949063+Ruan-Amorim@users.noreply.github.com>
2026-05-07 15:44:32 +0000

    databse js main

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 
+const API_TOKEN = "dawZ2bQ1wSy5WGKEQe1BCUYTYn6IYFty";
+const TABLE_ID = "1873778";
+
+const url = `api.baserow.io/api/database/rows/table/${TABLE_ID}/?user_field_names=true`;
+
+async function getApi() {
+    const resposta = await fetch(url, {
+        headers: {
+            Authorization: `Token ${API_TOKEN}`
+        }
+    })
+    const dados = await resposta.json();
+    const list = document.getElementById("Lista");
+    list.innerHTML = "";
+    dados.results.forEach(element => {
+        
+    });
+}
+
